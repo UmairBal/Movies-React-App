229af8467467b714c86f7e58a80ef669c6f3594b
Umair Azeem <86747265+UmairBal@users.noreply.github.com>
2024-11-07 03:22:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=478911a8';
 
 const App = () => {
     const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
+    const [searchTerm, setSearchTerm] = useState('spiderman');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
